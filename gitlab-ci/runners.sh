58981e39d62ea4418dc5700f74c196cf4c4ac5dc
vasiliev-alexey <alex_v81@MAIL.RU>
2020-05-20 15:49:41 +0300

    Gitlab ci 1 (#4)

---
+++ gitlab-ci/runners.sh
@@ -0,0 +1,20 @@
+!/bin/bash
+
+echo "param1=number"
+echo "param2=gitlab_url"
+echo "param3=gitlab_token"
+
+i=1
+while [ "$i" -le $1 ]; do
+  docker run -d --name gitlab-runner$i --restart always \
+  -v /srv/gitlab-runner$i/config:/etc/gitlab-runner \
+  -v /var/run/docker.sock:/var/run/docker.sock \
+  gitlab/gitlab-runner:latest
+
+  docker exec -it gitlab-runner$i gitlab-runner \
+  register --non-interactive --executor "docker" \
+  --docker-image alpine:latest --url "$2" --registration-token $3 \
+  --description "docker-runner"$i --tag-list "linux,xenial,ubuntu,docker" \
+  --run-untagged="true" --locked="false" --access-level="not_protected"
+  i=$(( i + 1 ))
+done
